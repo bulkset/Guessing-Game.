14e7094995a539d414b8ead8c85e41056b66bb75
ScaryTools <chupapa327@rambler.ru>
2023-10-25 14:08:05 +0500

    better version

--- main.c
+++ main.c
@@ -4,34 +4,40 @@
 
 int main()
 {
-    int num, secretNum = 20;
+    int num, secretNum = rand() % 100;
     char wannaPlay[4];
 
     printf("Hello welcome to the guess game.\n");
 
-    for (int i = 0; i < 5; i++) {
-        printf("Enter your guess:");
-        scanf("%d", &num);
+    while (1){
+        for (int i = 1; i < 6; i++) {
+            printf("Round %d: Enter your guess:", i);
+            scanf("%d", &num);
 
-        if (num > secretNum) {
-            printf("Your guess is greater, try again.\n");
+            if (num > secretNum) {
+                printf("Your guess is greater, try again.\n");
 
-        } else if (num < secretNum) {
-            printf("Your guess is less, try again.\n");
+            } else if (num < secretNum) {
+                printf("Your guess is less, try again.\n");
 
-        } else {
-            printf("You win!\n");
-            printf("Do you want to play again? (yes/no): ");
-            scanf("%s", wannaPlay);
-
-            if (strcmp(wannaPlay, "yes") == 0) {
-                secretNum = rand() % 100;
-                i = -1; // reset loop counter
             } else {
+                printf("You win!\n");
                 break;
             }
         }
+
+        printf("The secret number was: %d\n", secretNum);
+        printf("Do you want to play again? (yes/no): ");
+        scanf("%s", wannaPlay);
+
+        if (strcmp(wannaPlay, "yes") == 0) {
+            secretNum = rand() % 100;
+        } else {
+            break;
+        }
     }
+
+    printf("Thanks for playing!\n");
 
     return 0;
 }
